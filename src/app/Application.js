78e12fb4e8ec93996d013b38fba0c32e355dabb2
Yoana <yoni.vasileva8b@gmail.com>
2021-02-14 16:36:51 +0200

    done

--- src/app/Application.js
+++ src/app/Application.js
@@ -29,8 +29,37 @@ export default class Application extends EventEmitter {
    * and manipulate the DOM tree. Task data should be assigned to Application.data.
    * The APP_READY event should be emitted at the end of this method.
    */
+  async setData(){
+    const EventEmitter = require('events');
+    const app_ready = new EventEmitter();
+    const planets = [];
+    let response = await fetch('https://swapi.booost.bg/api/planets/');
+    let data = await response.json();
+
+    planets.push(data);
+
+    while(data.next){
+      response = await fetch(`${data.next}`);
+      data = await response.json();
+      planets.push(data);
+    }
+
+    const  planetsCollection = planets
+    .map((x) => x.results.map((c) => c)).flat();
+
+    this.data.count = planetsCollection.length;
+    this.data.planets = planetsCollection;
+
+    app_ready.emit('app is ready lol');
+  }
+
   async init() {
     // Initiate classes and wait for async operations here.
+    await this.setData();
+
+    app_ready.on('app is ready lol', function(){
+      console.log('something happening idk');
+    });
 
     this.emit(Application.events.APP_READY);
   }
